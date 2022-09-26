6c3fb7c2d61956cc091b0966c2595b6e8514644b
Sahil Choudhary <108992027+Sahilironrobots@users.noreply.github.com>
2022-09-26 17:34:35 +0000

    Initial commit

---
+++ Problem 3/problem3.js
@@ -0,0 +1,392 @@
+
+    $(document).ready(function()
+    {
+        $("#step1").click(function()
+        {
+            $(".step1").show("slow");
+            $(".step2").hide("slow");
+            $(".step3").hide("slow");
+            $(".step4").hide("slow");
+            $(".step5").hide("slow");
+        });
+    });
+    $(document).ready(function()
+    {
+        $("#step2").click(function()
+        {
+            $(".step2").show("slow");
+            $(".step1").hide("slow");
+            $(".step3").hide("slow");
+            $(".step4").hide("slow");
+            $(".step5").hide("slow");
+        });
+    });
+    $(document).ready(function()
+    {
+        $("#step3").click(function()
+        {
+            $(".step3").show("slow");
+            $(".step2").hide("slow");
+            $(".step1").hide("slow");
+            $(".step4").hide("slow");
+            $(".step5").hide("slow");
+        });
+    });
+
+    $(document).ready(function()
+    {
+        $("#step4").click(function()
+        {
+            $(".step4").show("slow");
+            $(".step2").hide("slow");
+            $(".step3").hide("slow");
+            $(".step1").hide("slow");
+            $(".step5").hide("slow");
+        });
+    });
+
+    $(document).ready(function()
+    {
+        $("#step5").click(function()
+        {
+            $(".step5").show("slow");
+            $(".step2").hide("slow");
+            $(".step3").hide("slow");
+            $(".step1").hide("slow");
+            $(".step4").hide("slow");
+        });
+    });
+
+        $(document).ready(function()
+        {
+            $("#home").click(function()
+            {
+                $(".menu1").show("slow");
+                $(".menu2").hide("slow");
+                $(".menu3").hide("slow");
+            });
+        });
+
+        $(document).ready(function()
+        {
+            $("#event").click(function()
+            {
+                $(".menu2").show("slow");
+                $(".menu1").hide("slow");
+                $(".menu3").hide("slow");
+            });
+        });
+
+        $(document).ready(function()
+        {
+            $("#register").click(function()
+            {
+                $(".menu3").show("slow");
+                $(".menu2").hide("slow");
+                $(".menu1").hide("slow");
+            });
+        });
+
+
+        $(document).ready(function()
+        {
+            $(".newsletter").click(function()
+            {
+                $("#form100").show("slow");
+            });
+            $(".newsletter").click(function()
+            {
+                $("#form99").hide("slow");
+                $("#form").hide("slow");
+                $("#form4").hide("slow");
+            });
+            
+        });
+        $(document).ready(function()
+        {
+            $(".speaker").click(function()
+            {
+                $("#form4").show("slow");
+            });
+            $(".speaker").click(function()
+            {
+                $("#form99").hide("slow");
+                $("#form").hide("slow");
+                $("#form100").hide("slow");
+            });
+            
+        });
+
+        $(document).ready(function()
+        {
+            $(".register").click(function()
+            {
+                $("#form").show("slow");
+            });
+            $("#bt3").click(function()
+            {
+                $("#form").hide("slow");
+            });
+            $(".register").click(function()
+            {
+                $("#form99").hide("slow");
+                $("#form4").hide("slow");
+                $("#form100").hide("slow");
+            });
+        });
+
+        $(document).ready(function()
+        {
+            $(".subscribe").click(function()
+            {
+                $("#form99").show("slow");
+            });
+            $(".subscribe").click(function()
+            {
+                $("#form").hide("slow");
+                
+                $("#form100").hide("slow");
+                $("#form4").hide("slow");
+            });
+           
+        });
+
+
+        $(document).ready(function() 
+            {
+               
+                $("#btn1").click(function() {
+                          $("#Expertise").show("slow");
+                          
+                });
+
+                $("#btn1").click(function() {
+                    $("#Contact").hide("slow");
+                          $("#Engage").hide("slow");
+                          $("#Experience").hide("slow");
+                          $("#Resources").hide("slow");
+                          $("#About").hide("slow");
+                          
+                });
+
+                $("#close").click(function()
+                {
+                    $("#Expertise").hide("slow");
+                })
+              
+            });
+
+            $(document).ready(function() 
+            {
+               
+                $("#btn2").click(function() {
+                          $("#Engage").show("slow");
+                          
+                });
+
+                $("#close1").click(function()
+                {
+                    $("#Engage").hide("slow");
+                })
+
+                $("#btn2").click(function() {
+                          $("#Expertise").hide("slow");
+                          $("Contact").hide("slow");
+                          $("#Experience").hide("slow");
+                          $("#Resources").hide("slow");
+                          $("#About").hide("slow");
+                          
+                });
+              
+            });
+
+            $(document).ready(function() 
+            {
+               
+                $("#btn3").click(function() {
+                          $("#Experience").show("slow");
+                          
+                });
+
+                $("#close2").click(function()
+                {
+                    $("#Experience").hide("slow");
+                })
+
+                $("#btn3").click(function() {
+                    $("#Engage").hide("slow");
+                          $("#Expertise").hide("slow");
+                          $("#Contact").hide("slow");
+                          $("#Resources").hide("slow");
+                          $("#About").hide("slow");
+                          
+                });
+              
+            });
+
+            $(document).ready(function() 
+            {
+               
+                $("#btn4").click(function() {
+                          $("#Resources").show("slow");
+                          
+                });
+
+                $("#close3").click(function()
+                {
+                    $("#Resource").hide("slow");
+                })
+
+                $("#btn4").click(function() {
+                    $("#Engage").hide("slow");
+                          $("#Expertise").hide("slow");
+                          $("#Experience").hide("slow");
+                          $("#Contact").hide("slow");
+                          $("#About").hide("slow");
+                          
+                });
+              
+            });
+
+            $(document).ready(function() 
+            {
+               
+                $("#btn5").click(function() {
+                          $("#About").show("slow");
+                          
+                });
+
+                $("#close4").click(function()
+                {
+                    $("#About").hide("slow");
+                })
+
+                $("#btn5").click(function() {
+                            $("#Engage").hide("slow");
+                          $("#Expertise").hide("slow");
+                          $("#Experience").hide("slow");
+                          $("#Resources").hide("slow");
+                          $("#Contact").hide("slow");
+                          
+                });
+              
+            });
+            
+            $(document).ready(function() 
+            {
+               
+                $("#btn6").click(function() {
+                          $("#Contact").show("slow");
+                          
+                });
+
+                $("#close5").click(function()
+                {
+                    $("#Contact").hide("slow");
+                })
+
+                $("#btn6").click(function() {
+                          $("#Engage").hide("slow");
+                          $("#Expertise").hide("slow");
+                          $("#Experience").hide("slow");
+                          $("#Resources").hide("slow");
+                          $("#About").hide("slow");
+                          
+                });
+              
+            });
+            $(document).ready(function()
+            {
+                $("#sciimg").click(function()
+                {
+                    $("#sci1").show("slow");
+                    
+                    $("#sci2").hide("slow");
+                    $("#sci3").hide("slow");
+                });
+            });
+            $(document).ready(function()
+            {
+                $("#sciimg1").click(function()
+                {
+                    $("#sci2").show("slow");
+                   
+                    $("#sci1").hide("slow");
+                    $("#sci3").hide("slow");
+                });
+            });
+            $(document).ready(function()
+            {
+                $("#sciimg2").click(function()
+                {
+                    $("#sci3").show("slow");
+                  
+                    $("#sci2").hide("slow");
+                    $("#sci1").hide("slow");
+                });
+            });
+            
+            $(document).ready(function()
+            {
+                $("#attimg").click(function()
+                {
+                    $("#att1").show("slow");
+                  
+                    $("#att2").hide("slow");
+                    $("#att3").hide("slow");
+                });
+            });
+
+            $(document).ready(function()
+            {
+                $("#attimg1").click(function()
+                {
+                    $("#att2").show("slow");
+                  
+                    $("#att1").hide("slow");
+                    $("#att3").hide("slow");
+                });
+            });
+
+            $(document).ready(function()
+            {
+                $("#attimg2").click(function()
+                {
+                    $("#att3").show("slow");
+                  
+                    $("#att1").hide("slow");
+                    $("#att2").hide("slow");
+                });
+            });
+
+            $(document).ready(function()
+            {
+                $("#compimg").click(function()
+                {
+                    $("#comp1").show("slow");
+                  
+                    $("#comp2").hide("slow");
+                    $("#comp3").hide("slow");
+                });
+            });
+            $(document).ready(function()
+            {
+                $("#compimg1").click(function()
+                {
+                    $("#comp2").show("slow");
+                  
+                    $("#comp3").hide("slow");
+                    $("#comp1").hide("slow");
+                });
+            });
+            $(document).ready(function()
+            {
+                $("#compimg2").click(function()
+                {
+                    $("#comp3").show("slow");
+                  
+                    $("#comp2").hide("slow");
+                    $("#comp1").hide("slow");
+                });
+            });
+ 
